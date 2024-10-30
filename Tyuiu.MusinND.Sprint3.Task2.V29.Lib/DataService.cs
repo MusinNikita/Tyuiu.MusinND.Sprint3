e72ee92ed077de73e81d51e107f3014d99cfcd93
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 02:52:30 +0500

    12121

--- Tyuiu.MusinND.Sprint3.Task2.V29.Lib/DataService.cs
+++ Tyuiu.MusinND.Sprint3.Task2.V29.Lib/DataService.cs
@@ -8,12 +8,12 @@ namespace Tyuiu.MusinND.Sprint3.Task2.V29.Lib
         {
             double sum = 0;
 
-            for (int k = 1; k <= 18; k++)
+            for (int k = startValue; k <= stopValue; k++)
             {
-                sum += (Math.Pow(a, k) + 0.25) * Math.Cos(k);
+                sum += (Math.Pow(value, k) + 0.25) * Math.Cos(k);
             }
 
-            return Math.Round(sum, 3);
+            return Math.Round(sum, 2);
         }
     }
 }
