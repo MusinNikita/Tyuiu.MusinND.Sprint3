e8167f5023ef2c629504771899c496a816f0a62a
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 02:37:56 +0500

    1111111111

--- Tyuiu.MusinND.Sprint3.Task0.V11.Lib/DataService.cs
+++ Tyuiu.MusinND.Sprint3.Task0.V11.Lib/DataService.cs
@@ -2,18 +2,15 @@
 
 namespace Tyuiu.MusinND.Sprint3.Task0.V11.Lib
 {
-    public class DataService : ISprint3Task0V11
+    public double GetSumSeries(int value, int startValue, int stopValue)
     {
-        public double GetSumSeries(int value, int startValue, int stopValue)
+        double sum = 0;
+
+        for (int k = startValue; k <= stopValue; k++)
         {
-            double sum = 0;
-
-            for (int k = startValue; k <= stopValue; k++)
-            {
-                sum += Math.Pow(1.0 / (value * k), 2);
-            }
-
-            return sum;
+            sum += Math.Pow(1.0 / (value * k), 2);
         }
+
+        return sum;
     }
 }
