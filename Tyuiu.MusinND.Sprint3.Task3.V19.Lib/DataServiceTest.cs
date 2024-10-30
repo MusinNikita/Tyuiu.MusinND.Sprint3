f3f5790bd2d2becb91aad82874e20beaba2571e1
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 02:57:47 +0500

    121212

---
+++ Tyuiu.MusinND.Sprint3.Task3.V19.Lib/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MusinND.Sprint3.Task3.V19.Lib
+{
+    public class DataServiceTest : ISprint3Task3V19
+    {
+        public string ReplaceCharOnNum(string value, char replaceable, char replacement)
+        {
+            string result = "";
+
+            foreach (char c in value)
+            {
+                if (c == replaceable)
+                {
+                    result += replacement;
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint3.Task3.V19.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.MusinND.Sprint3.Task3.V19.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
