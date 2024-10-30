39a20bbcf6732d90db31df12fcff5c90a5934a38
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 02:51:28 +0500

    1212

---
+++ Tyuiu.MusinND.Sprint3.Task2.V29.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MusinND.Sprint3.Task2.V29.Lib
+{
+    public class DataService : ISprint3Task2V29
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double sum = 0;
+
+            for (int k = 1; k <= 18; k++)
+            {
+                sum += (Math.Pow(a, k) + 0.25) * Math.Cos(k);
+            }
+
+            return Math.Round(sum, 3);
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint3.Task2.V29.Test/DataService.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.MusinND.Sprint3.Task2.V29.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
