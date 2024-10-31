094d6b26de676ffc4cf237edfec027b0de1c3c1c
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 20:48:20 +0500

    121212

---
+++ Tyuiu.MusinND.Sprint3.Task6.V28.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MusinND.Sprint3.Task6.V28.Lib
+{
+    public class DataService : ISprint3Task6V28
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int totalCount = 0;
+
+            // Перебираем все числа в заданном диапазоне
+            for (int num = startValue; num <= stopValue; num++)
+            {
+                // Считаем делители меньше 7
+                for (int i = 1; i < 7; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        totalCount++;
+                    }
+                }
+            }
+
+            return totalCount;
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint3.Task6.V28.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.MusinND.Sprint3.Task6.V28.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
