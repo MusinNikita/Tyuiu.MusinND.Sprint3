61d85bda1a8aab6882d938b6494fe8ec5f931865
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 20:37:35 +0500

    1111111111111

--- Tyuiu.MusinND.Sprint3.Task5.V8.Lib/DataService.cs
+++ Tyuiu.MusinND.Sprint3.Task5.V8.Lib/DataService.cs
@@ -4,18 +4,18 @@ namespace Tyuiu.MusinND.Sprint3.Task5.V8.Lib
 {
     public class DataService : ISprint3Task5V8
     {
-        public double GetSumSumSeries(int startValue1, int startValue2, int stopValue1, int stopValue2)
+        public double GetSumSumSeries(int startValue1 = 1, int startValue2 = 1, int stopValue1 = 3, int stopValue2 = 12)
         {
-            double totalSum = 0;
+            double innerSum = 0;
 
-            // Считаем внутреннюю сумму
+            // Внутренний цикл от startValue2 до stopValue2 (1 до 12)
             for (int k = startValue2; k <= stopValue2; k++)
             {
-                totalSum += 1 / Math.Sin(k); // Суммируем 1/sin(k)
+                innerSum += 1 / Math.Sin(k); // Суммируем 1/sin(k)
             }
 
-            // Умножаем сумму на 3 и добавляем 3
-            totalSum = totalSum * (stopValue1 - startValue1 + 1) + 3; // Умножаем на количество итераций по i (3)
+            // Умножаем внутреннюю сумму на количество итераций по i и добавляем 3
+            double totalSum = (innerSum * (stopValue1 - startValue1 + 1)) + 3;
 
             return totalSum; // Возвращаем результат
         }
