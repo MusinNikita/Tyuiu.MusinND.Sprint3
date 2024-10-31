30a8a78808afc17eb7a2fa2d033d849597556ee9
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 20:43:58 +0500

    1212121212

--- Tyuiu.MusinND.Sprint3.Task5.V8.Lib/DataService.cs
+++ Tyuiu.MusinND.Sprint3.Task5.V8.Lib/DataService.cs
@@ -14,8 +14,8 @@ namespace Tyuiu.MusinND.Sprint3.Task5.V8.Lib
                 innerSum += 1 / Math.Sin(k); // Суммируем 1/sin(k)
             }
 
-            // Умножаем внутреннюю сумму на количество итераций по i (stopValue1) и добавляем 3
-            double totalSum = (innerSum * (stopValue1 - startValue1 + 1)) + 3;
+            // Умножаем внутреннюю сумму на 3 (количество итераций по i) и добавляем 3
+            double totalSum = (innerSum * (stopValue1 - startValue1 + 1)) + 3; // 3 итерации по i
 
             return totalSum; // Возвращаем результат
         }
